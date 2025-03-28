150b7c2d1b9159dcc7815738c5b1166db6b6f92b
Dillon Johnson <me@nollidnosnhoj.com>
2025-03-27 21:59:30 -1000

    update

--- .config/hypr/scripts/reset_xdg.sh
+++ .config/hypr/scripts/reset_xdg.sh
@@ -1,20 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 sleep 1
 killall xdg-desktop-portal-hyprland
-killall xdg-desktop-portal-gnome
-killall xdg-desktop-portal-kde
-killall xdg-desktop-portal-lxqt
-killall xdg-desktop-portal-wlr
+killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
-sleep 1
-
-# Use different directory on NixOS
-if [ -d /run/current-system/sw/libexec ]; then
-    libDir=/run/current-system/sw/libexec
-else
-    libDir=/usr/lib
-fi
-
-$libDir/xdg-desktop-portal-hyprland &
+/usr/lib/xdg-desktop-portal-hyprland &
 sleep 2
-$libDir/xdg-desktop-portal &
+/usr/lib/xdg-desktop-portal &
