c3346d83a0530fc1ff6bedaca29ad2e09bdd8048
Dillon Johnson <me@nollidnosnhoj.com>
2025-04-29 15:34:35 -1000

    refactor hyprland

--- hypr/.config/hypr/scripts/reset_xdg.sh
+++ hypr/.config/hypr/scripts/reset_xdg.sh
@@ -3,6 +3,9 @@ sleep 1
 killall xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
+sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
+/usr/libexec/xdg-desktop-portal-hyprland &
 sleep 2
 /usr/lib/xdg-desktop-portal &
+/usr/libexec/xdg-desktop-portal &
