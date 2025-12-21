a5c0d2a1d33b0e093e14ce103204a5028371cf0d
Dillon Johnson <me@nollidnosnhoj.com>
2025-12-20 15:50:34 -1000

    update fish

--- fish/.config/fish/conf.d/40-integrations.fish
+++ fish/.config/fish/conf.d/40-integrations.fish
@@ -1,4 +1,6 @@
-keychain --eval --quiet id_ed25519 id_rsa
+if status is-interactive
+    keychain --eval id_ed25519 id_rsa | source
+end
 fzf --fish | source
 zoxide init fish | source
 mise activate fish | source
--- fish/.config/fish/config.fish
+++
@@ -1,4 +0,0 @@
-# loop through the ./configs fish script and source them
-for config in ~/.config/fish/configs/*.fish
-    source $config
-end
